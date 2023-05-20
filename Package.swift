e5171875f0ef4efbfca73938dfa019dbe28e4e1f
Adis Veletanlic <96535657+adisve@users.noreply.github.com>
2023-05-20 16:27:11 +0200

    Change supported min version iOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "PillPickerView",
     platforms: [
-        .iOS(.v15), .macOS(.v11)
+        .iOS(.v14), .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
