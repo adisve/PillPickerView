7262156fb1c4cbdeef21f946fb56457de213e047
Adis Veletanlic <96535657+adisve@users.noreply.github.com>
2023-05-20 16:21:23 +0200

    Move PillPickerView to root of Source/

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PillPickerView",
-            dependencies: []),
-        
+            dependencies: [],
+            path: "Sources"
+        ),
     ]
 )
